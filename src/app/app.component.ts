090e141dc7428dc38a8e274dd7dbbf40f030c36a
cpandares <>
2022-08-23 18:16:26 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,77 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fundamentos-eccomerce';
+  name = 'Cesar';
+  age = 25;
+  /* btnDisabled = this.age < 18 ? true : false; */
+  btnDisabled = true;
+
+  names :string[] = ['cesar', 'enrique', 'manuel'];
+  newName : string = '';
+
+
+  register = {
+    name:'',
+    email:'',
+    password:''
+  }
+
+
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/headphones_b_1.webp',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/headphones_b_2.webp'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/headphones_b_3.webp'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/headphones_b_4.webp'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/headphones_b_3.webp'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/headphones_c_1.webp'
+    }
+  ]
+
+
+  toggleButton(){
+
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  agetIncrement(){
+      this.age += 1
+  }
+
+  addName (){
+    this.names.push(this.newName);
+    this.newName= ''
+  }
+
+  deleteName(index:number){
+    this.names.splice(index, 1)
+  }
+
+  onRegister(){
+    console.log(this.register)
+  }
+
 }
